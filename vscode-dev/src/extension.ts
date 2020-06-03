ec925e2f21edf496bcea89fcbd4c6cef40b65df6
stm32p103 <stm32p103@gmail.com>
2020-06-04 01:20:46 +0900

    Angular Webview表示

--- vscode-dev/src/extension.ts
+++ vscode-dev/src/extension.ts
@@ -1,27 +1,35 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as path from 'path';
+import * as fs from 'fs';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "vscodedev" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vscodedev.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+	// dummy command
+	let disposable = vscode.commands.registerCommand('vscodedev.activate', ()=>{});
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from VscodeDev!');
-	});
+	vscode.commands.registerCommand('vscodedev.showWebview', () => {
+        const rootPath = path.join( context.extensionPath, 'public' );
+		const panel = vscode.window.createWebviewPanel( 'vscodedev.webview', 'Webview', vscode.ViewColumn.One,{
+			localResourceRoots: [ vscode.Uri.file( rootPath ) ],
+			enableScripts: true,
+			retainContextWhenHidden: true
+		} );
 
+		const htmlPath = path.join( rootPath, 'index.html' );
+        const rootUri = vscode.Uri.file( rootPath );
+        const tmp = fs.readFileSync( htmlPath, { encoding: 'utf8' } );
+		
+		// modify <base>
+		panel.webview.html = tmp.replace('<base href="/">', `<base href="${rootUri.with({scheme:'vscode-resource'}).toString()}/">`);
+
+		panel.webview.onDidReceiveMessage( msg => {
+			console.log( msg );
+		} );
+	}, disposable );
+
+	
 	context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
