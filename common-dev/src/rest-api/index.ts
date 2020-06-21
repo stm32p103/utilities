f70889908fcb618c3f8f0dfefea29100a4da5976
stm32p103 <stm32p103@gmail.com>
2020-06-22 00:43:36 +0900

    足回り変更

--- common-dev/src/rest-api/index.ts
+++ common-dev/src/rest-api/index.ts
@@ -1,4 +1,1 @@
-export * from './connection';
 export * from './rest-api';
-export * from './basic-auth-connection';
-export * from './jira';
--- common-dev/src/rest-api/specifier.ts
+++
@@ -1,7 +0,0 @@
-
-export class Specifier<T extends string>{
-  constructor( private readonly type: T, private readonly target: string ) {}
-  get value() {
-    return { [this.type]: this.target };
-  }
-}
