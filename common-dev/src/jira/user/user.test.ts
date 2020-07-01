38c65fc38387c615c91c6d2450b014eceaef839c
stm32p103 <stm32p103@gmail.com>
2020-07-01 22:28:05 +0900

    test更新

--- common-dev/src/jira/user/user.test.ts
+++ common-dev/src/jira/user/user.test.ts
@@ -7,42 +7,54 @@ const api = new RestAPI( new URL( HOST ) );
 api.configureAuth( CREDENTIAL );
 
 const jira = new Jira( api );
+const keyword = CREDENTIAL.username.substr(0,3);
+let userKey: string;
 
-let userId: string;
+function checkKeyExists( key: string, users: { key: string }[] ) {
+  const index = users.findIndex( u => u.key == userKey );
+  expect( index ).toBeGreaterThanOrEqual(0);
+}
 
 test('get', async () => {
   const user = await jira.user.get( { username: CREDENTIAL.username } );
-  console.log( getKeys( user ) );
-  console.log( user );
+  userKey = user.key;
+  // console.log( getKeys( users[0] ) );
+  // console.log( user );
   expect( user.name ).toBe( CREDENTIAL.username );
 } );
 
 test('find bulk assignable users', async () => {
   const users = await jira.user.findBulkAssignable( { projectKeys: PROJECTKEYS } );
-  console.log( getKeys( users[0] ) );
+  // console.log( getKeys( users[0] ) );
+  checkKeyExists( userKey, users );
 } );
 
 test('find assignable users', async () => {
-  const users = await jira.user.findAssignable( { project: 'API' } );
-  console.log( getKeys( users[0] ) );
+  const users = await jira.user.findAssignable( { project: PROJECTKEYS[0] } );
+  // console.log( users[0] );
+  checkKeyExists( userKey, users );
 } );
 
 test('find user with all permissions', async () => {
-  const users = await jira.user.findUsersWithAllPermissions( { permissions: [ 'CLOSE_ISSUE' ], issueKey: 'SMP-1' } );
-  console.log( getKeys( users[0] ) );  
+  const users = await jira.user.findUsersWithAllPermissions( { permissions: [ 'CLOSE_ISSUE' ], issueKey: `${PROJECTKEYS[0]}-1` } );
+  // console.log( users[0] );
+  checkKeyExists( userKey, users );
 } );
 
 test('find user for picker', async () => {
-  const users = await jira.user.findUsersForPicker( { query: 'adm', showAvatar: true } );
-  console.log( users );  
+  const picker = await jira.user.findUsersForPicker( { query: keyword, showAvatar: true } );
+  // console.log( picker );
+  checkKeyExists( userKey, picker.users );
 } );
 
 test('find user', async () => {
-  const users = await jira.user.findUsers( { username: 'adm' } );
-  console.log( users[0] ); 
+  const users = await jira.user.findUsers( { username: keyword } );
+  // console.log( users[0] );
+  checkKeyExists( userKey, users );
 } );
 
 test('find user with browse permission', async () => {
-  const users = await jira.user.findUsersWithBrowsePermission( { username: 'adm', issueKey: 'API-1' } );
-  console.log( users[0] ); 
+  const users = await jira.user.findUsersWithBrowsePermission( { username: keyword, issueKey: 'API-1' } );
+  // console.log( users[0] );
+  checkKeyExists( userKey, users );
 } );
